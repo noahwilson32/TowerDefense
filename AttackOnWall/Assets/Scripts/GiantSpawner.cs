fed2096cda59424572bfe24ca86d010dcc425037
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-06-16 19:41:16 -0400

    Working on Giant Spawner

---
+++ AttackOnWall/Assets/Scripts/GiantSpawner.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GiantSpawner : MonoBehaviour
+{
+    Vector3Int moveMag;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MoveSpawner();
+    }
+    public void MoveSpawner()
+    {
+        if (transform.position.y >= 6)
+        {
+            moveMag = new Vector3Int(0, -1, 0);
+        }
+        if(transform.position.y <= -3)
+        {
+            moveMag = new Vector3Int(0, 1, 0);
+        }
+        transform.position += moveMag;
+        
+
+    }
+}
