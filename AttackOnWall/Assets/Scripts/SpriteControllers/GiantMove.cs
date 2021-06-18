2c243056c8d11570676d446b41c7a0ee75619999
NoahWilson <noahwilsonmailbox32@gmail.com>
2021-06-18 11:35:05 -0400

    Giant stop at walls

--- AttackOnWall/Assets/Scripts/SpriteControllers/GiantMove.cs
+++ AttackOnWall/Assets/Scripts/SpriteControllers/GiantMove.cs
@@ -1,11 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GiantMove : MonoBehaviour
 {
     public float moveSpeed = 1.5f;
 
+    public Tilemap map;
+    public TileBase[] wallTiles;
+
+    public TileBase currentTile;
     // Start is called before the first frame update
     void Start()
     {
@@ -21,5 +26,16 @@ public class GiantMove : MonoBehaviour
     {
         Vector2 velocity = new Vector2(transform.position.x - (moveSpeed * Time.deltaTime), transform.position.y);
         transform.position = velocity;
+
+        Vector3Int pos = new Vector3Int((int)transform.position.x - 1, (int)transform.position.y, (int)transform.position.z);
+        currentTile = map.GetTile(pos);
+
+        foreach(TileBase wall in wallTiles)
+        {
+            if(currentTile == wall)
+            {
+                moveSpeed = 0;
+            }
+        }
     }
 }
